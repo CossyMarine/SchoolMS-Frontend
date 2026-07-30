a3c51262353ccac6c972b4596bc474802506176d
CossyMarine <shidayawatukibwari@gmail.com>
2026-07-30 23:09:42 +0300

    Delete src/pages/StorekeeperPage.jsx

--- src/pages/StorekeeperPage.jsx
+++
@@ -1,9 +0,0 @@
-import ProductManagement from "../components/Storekeeper/ProductManagement";
-
-export default function StorekeeperPage() {
-  return (
-    <div className="min-h-screen bg-gray-50 p-4 sm:p-6">
-      <ProductManagement />
-    </div>
-  );
-}
